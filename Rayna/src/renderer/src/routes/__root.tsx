5652dadd88bc88ed63a20f72f9d6cc1900e45e82
ibb2 <70052141+ibb2@users.noreply.github.com>
2025-11-30 09:50:28 +1100

    Installed/update tanstack query and its eslint configs

--- Rayna/src/renderer/src/routes/__root.tsx
+++ Rayna/src/renderer/src/routes/__root.tsx
@@ -1,9 +1,17 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const queryClient = new QueryClient()
 
 export const Route = createRootRoute({
   component: () => (
     <div className="flex flex-1 flex-col overflow-hidden h-full">
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </div>
   )
 })
