9a991f681fc45da8675a8c853aba7ba280bdd1f7
ibb2 <70052141+ibb2@users.noreply.github.com>
2026-01-26 17:07:05 +1100

    Update className covention.

--- src/renderer/src/components/app-sidebar.tsx
+++ src/renderer/src/components/app-sidebar.tsx
@@ -114,7 +114,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
-            <SidebarMenuButton asChild className="data-[slot=sidebar-menu-button]:!p-1.5">
+            <SidebarMenuButton asChild className="data-[slot=sidebar-menu-button]:p-1.5!">
               <div className="flex flex-row gap-4">
                 <AudioWaveform className="size-5!" />
                 <span className="text-base font-semibold group-data-[collapsible=icon]:hidden">
