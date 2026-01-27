d7279c524594df45c928d440739689436486aa68
ibb2 <70052141+ibb2@users.noreply.github.com>
2026-01-27 12:11:16 +1100

    Initializing backend when user selects server.
    Should resolve issue with new applications not connecting to the backend
    until it is closed and reopened.

--- src/renderer/src/routes/app/route.tsx
+++ src/renderer/src/routes/app/route.tsx
@@ -10,42 +10,6 @@ export const Route = createFileRoute('/app')({
 
 function AppLayoutComponent() {
 
-  // const initializeFastApiBackend = async () => {
-  //   let server: PlexServer | null = await window.api.auth.getUserSelectedServer()
-
-  //   console.log("Server: ", server)
-
-  //   if (server == null) {
-  //     console.log("Server is null")
-  //     navigate({
-  //               to: '/server',
-  //               replace: true,
-  //               reloadDocument: true,
-  //             })
-  //     return
-  //   }
-
-  //   const accessToken = await window.api.auth.getUserAccessToken()
-  //   console.log('serverUrl:', server.connections)
-
-  //   const response = await fetch(`http://127.0.0.1:11222/init`, {
-  //     method: 'POST',
-  //     headers: {
-  //       Authorization: `Bearer ${accessToken}`,
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify({
-  //       serverUrl: server.connections[0].uri
-  //     })
-  //   })
-  //   await response.json()
-  //   onLoad(true)
-  // }
-
-  // useEffect(() => {
-  //   if (!load) initializeFastApiBackend()
-  // }, [load])
-
   return (
     <div className="flex flex-col h-screen w-screen">
       <div className="flex-1 min-h-0 overflow-hidden relative">
