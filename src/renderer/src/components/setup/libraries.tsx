770d4863c449cf764cb4a6c711cd17b9db931456
ibb2 <70052141+ibb2@users.noreply.github.com>
2026-02-01 22:04:45 +1100

    Display library items properly

--- src/renderer/src/components/setup/libraries.tsx
+++ src/renderer/src/components/setup/libraries.tsx
@@ -1,46 +1,65 @@
-import { Spinner } from "@/components/ui/spinner"
-import { useQuery } from "@tanstack/react-query"
+import { Button } from "@/components/ui/button";
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemMedia,
+  ItemTitle,
+} from "@/components/ui/item";
+import { Spinner } from "@/components/ui/spinner";
+import { useQuery } from "@tanstack/react-query";
+import { BadgeCheckIcon, Check, ChevronRightIcon, Music } from "lucide-react";
 
-
-export default function Libraries({progress}) {
-
+export default function Libraries({ progress }) {
   const { isPending, error, data } = useQuery({
-    queryKey: ['libraries'],
+    queryKey: ["libraries"],
     queryFn: async () => {
-      const res = await fetch('http://127.0.0.1:34567/library/sections/all')
-      if (!res.ok) throw new Error('Failed to fetch libraries')
-      return res.json()
+      const res = await fetch("http://127.0.0.1:34567/library/sections/all");
+      if (!res.ok) throw new Error("Failed to fetch libraries");
+      return res.json();
     },
     staleTime: 30 * 60 * 1000,
-    retry: true
-  })
+    retry: true,
+  });
 
-  if (
-    isPending
-  )
+  if (isPending)
     return (
       <div className="flex items-center justify-center w-full h-full">
         <Spinner className="size-8" />
       </div>
-    )
+    );
 
-  if (error )
-    return (
-      'An error has occurred: ' + error?.message 
-    )
-
+  if (error) return "An error has occurred: " + error?.message;
 
   return (
     <div>
       <p>Select your libraries</p>
       <div>
         {data.map((library) => (
-          <div>
-            <p>1</p>
-            <p>{library.title}</p>
+          <>
+            <div
+              className="flex w-full max-w-md flex-col gap-6"
+              key={library.uuid}
+            >
+              <Item variant="outline" size="sm" asChild>
+                <a href="#">
+                  <ItemMedia>
+                    <Music className="size-5" />
+                  </ItemMedia>
+                  <ItemContent>
+                    <ItemTitle>{library.title}</ItemTitle>
+                    <ItemDescription>{library.type}</ItemDescription>
+                  </ItemContent>
+                  <ItemActions>
+                    <Check className="size-4" />
+                  </ItemActions>
+                </a>
+              </Item>
             </div>
+          </>
         ))}
       </div>
     </div>
-  )
+  );
 }
