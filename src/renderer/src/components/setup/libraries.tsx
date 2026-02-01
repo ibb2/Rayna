921dcddae8f4b1583b1c618597dcdaacbad7e8be
ibb2 <70052141+ibb2@users.noreply.github.com>
2026-02-01 21:31:38 +1100

    Libraries returning correctly to frontend.

--- src/renderer/src/components/setup/libraries.tsx
+++ src/renderer/src/components/setup/libraries.tsx
@@ -1,43 +1,46 @@
-import { Button } from "@/components/ui/button"
 import { Spinner } from "@/components/ui/spinner"
 import { useQuery } from "@tanstack/react-query"
 
 
 export default function Libraries({progress}) {
 
-  const testApi = () => {
-     fetch('http://127.0.0.1:34567/library/sections/all')
-  }
+  const { isPending, error, data } = useQuery({
+    queryKey: ['libraries'],
+    queryFn: async () => {
+      const res = await fetch('http://127.0.0.1:34567/library/sections/all')
+      if (!res.ok) throw new Error('Failed to fetch libraries')
+      return res.json()
+    },
+    staleTime: 30 * 60 * 1000,
+    retry: true
+  })
 
-  // const queryMusicLibraries = useQuery({
-  //   queryKey: ['libraries'],
-  //   queryFn: async () => {
+  if (
+    isPending
+  )
+    return (
+      <div className="flex items-center justify-center w-full h-full">
+        <Spinner className="size-8" />
+      </div>
+    )
 
-  //     // const accessToken = await window.api.auth.getUserAccessToken()
-
-  //     fetch('http://127.0.0.1:34567/library/sections/all').then((res) => {
-  //       if (!res.ok) throw new Error('Network response was not ok')
-  //       return res.json()
-  //     })},
-  //   staleTime: 30 * 60 * 1000,
-  //   retry: true
-  // })
-
-  // if (
-  //   queryMusicLibraries.isLoading
-  // )
-  //   return (
-  //     <div className="flex items-center justify-center w-full h-full">
-  //       <Spinner className="size-8" />
-  //     </div>
-  //   )
-  // if (
-  //   queryMusicLibraries.isError 
-  // )
-  //   return (
-  //     'An error has occurred: ' + queryMusicLibraries.error?.message 
-  //   )
+  if (error )
+    return (
+      'An error has occurred: ' + error?.message 
+    )
 
 
-  return <div>Select your libraries <Button onClick={() => testApi()} /></div>
+  return (
+    <div>
+      <p>Select your libraries</p>
+      <div>
+        {data.map((library) => (
+          <div>
+            <p>1</p>
+            <p>{library.title}</p>
+            </div>
+        ))}
+      </div>
+    </div>
+  )
 }
