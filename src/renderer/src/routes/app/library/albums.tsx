040f2c4247a17db7e8bfbde666032ad12931c0a2
ibb2 <70052141+ibb2@users.noreply.github.com>
2026-02-07 20:01:45 +1100

    Populating album page with real data.

--- src/renderer/src/routes/app/library/albums.tsx
+++ src/renderer/src/routes/app/library/albums.tsx
@@ -1,41 +1,58 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardHeader,
-  CardAction,
-  CardTitle,
-  CardDescription,
-  CardFooter,
-} from "@/components/ui/card";
+import { Spinner } from "@/components/ui/spinner";
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import { Badge } from "lucide-react";
+import BlankImage from "@/assets/512px-Black_colour.jpg";
 
 export const Route = createFileRoute("/app/library/albums")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const albums = useQuery({
+    queryKey: ["albums"],
+    queryFn: () =>
+      fetch("http://127.0.0.1:34567/music/albums/all").then((res) => {
+        if (!res.ok) throw new Error("Network response was not ok");
+        return res.json();
+      }),
+  });
+
+  if (albums.isLoading)
+    return (
+      <div className="flex items-center justify-center w-full h-full">
+        <Spinner className="size-8" />
+      </div>
+    );
+  if (albums.isError) return "An error has occurred: " + albums.error?.message;
+
   return (
     <div className="flex flex-col h-full overflow-y-auto mb-20 px-6">
       <div>
         {/* Header */}
-        <p>Albums</p>
-        <p>count {37}</p>
+        {/* <p>Albums</p>
+        <p>count {37}</p> */}
       </div>
-      <div>{/* Filters */}</div>
-      <div className="flex flex-wrap gap-x-8 w-full gap-y-12 pb-4">
+      <div>
+        {/* Filters */}
+        {/* <p></p> */}
+      </div>
+      <div className="flex flex-wrap gap-x-8 w-full gap-y-8 pb-4">
         {/* Content */}
-        {[0, 1, 2, 3, 4, 5, 7, 8, 9, 10].map((_) => (
-          <div className="flex flex-col gap-2 max-w-fit">
+        {albums.data.map((album) => (
+          <div className="flex flex-col gap-2 max-w-sm">
             <img
-              src="https://avatar.vercel.sh/shadcn1"
+              src={album.thumb ?? BlankImage}
               alt="Event cover"
-              className="relative z-20 aspect-video rounded-lg size-36 object-cover brightness-60 grayscale dark:brightness-40"
+              className="relative z-20 aspect-video rounded-lg size-36 object-cover"
             />
-            <p className="hover:underline font-semibold text-sm">
-              Album artist
-            </p>
-            <p>Album title</p>
+            <div className="w-36">
+              <p className="truncate hover:underline max-w-sm text-sm">
+                {album.title}
+              </p>
+              <p className="hover:underline font-semibold text-xs">
+                {album.artist}
+              </p>
+            </div>
           </div>
         ))}
       </div>
