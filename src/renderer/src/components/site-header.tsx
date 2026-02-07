0e0843d0540a179b49a89d54565ab25af6405711
ibb2 <70052141+ibb2@users.noreply.github.com>
2026-02-07 18:54:54 +1100

    Sidebar collapse button inline with rest of content.

--- src/renderer/src/components/site-header.tsx
+++ src/renderer/src/components/site-header.tsx
@@ -6,7 +6,7 @@ export function SiteHeader() {
   return (
     <header className="flex h-(--header-height) shrink-0 items-center mt-1.5 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        <SidebarTrigger className="-ml-1" />
+        <SidebarTrigger />
         <Separator
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
